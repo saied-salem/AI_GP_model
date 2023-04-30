7c31aeac86a35a9bb3570856a1cfe5f6b063ab73
Yasien Essam <yasien.essam99@gmail.com>
2023-04-30 07:00:43 +0200

    edit name of number of classes like monai

--- Models/basic_unet.py
+++ Models/basic_unet.py
@@ -5,7 +5,7 @@ class UNet(nn.Module):
     def __init__(self, n_channels, n_classes, bilinear=False):
         super(UNet, self).__init__()
         self.n_channels = n_channels
-        self.n_classes = n_classes
+        self.output_channels = n_classes
         self.bilinear = bilinear
 
         self.inc = (DoubleConv(n_channels, 64))
